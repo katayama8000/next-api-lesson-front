6a342cd7492b34dbf021ec69a0ec0ad034dbc450
katayama <tattu.0310@gmail.com>
2022-08-27 21:02:13 +0900

    🎉 first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,45 @@
 import "src/lib/tailwind.css";
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
-import Head from "next/head";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import axios from "axios";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
+  axios.defaults.withCredentials = true;
+  useEffect(() => {
+    const getCsrfToken = async () => {
+      const { data } = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/csrf`
+      );
+      axios.defaults.headers.common["csrf-token"] = data.csrfToken;
+    };
+    getCsrfToken();
+  }, []);
   return (
-    <>
-      <Head>
-        <title>Next.js</title>
-      </Head>
-      <MantineProvider withGlobalStyles withNormalizeCSS>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "dark",
+          fontFamily: "Verdana, sans-serif",
+        }}
+      >
         <Component {...pageProps} />
       </MantineProvider>
-    </>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
