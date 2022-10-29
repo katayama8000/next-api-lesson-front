1908ff24fe3e810695c7114dde0722ff3cee3bdf
katayama <tattu.0310@gmail.com>
2022-10-29 14:29:57 +0900

    update

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,33 @@
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import axios from "axios";
+import { useQueryClient } from "@tanstack/react-query";
+import { Layout } from "../components/Layout";
+import { LogoutIcon } from "@heroicons/react/solid";
+// import { UserInfo } from "../components/UserInfo";
+// import { TaskForm } from "../components/TaskForm";
+// import { TaskList } from "../components/TaskList";
+
+const Dashboard: NextPage = () => {
+  const { push } = useRouter();
+  const queryClient = useQueryClient();
+  const logout = async () => {
+    queryClient.removeQueries(["tasks"]);
+    queryClient.removeQueries(["user"]);
+    await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`);
+    push("/");
+  };
+  return (
+    <Layout title="Task Board">
+      <LogoutIcon
+        className="mb-6 h-6 w-6 cursor-pointer text-blue-500"
+        onClick={logout}
+      />
+      {/* <UserInfo />
+      <TaskForm />
+      <TaskList /> */}
+    </Layout>
+  );
+};
+
+export default Dashboard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,115 @@
 import type { NextPage } from "next";
-import { Sample } from "@component/Sample";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
+import * as Yup from "yup";
+import { IconDatabase } from "@tabler/icons";
+import { ShieldCheckIcon } from "@heroicons/react/solid";
+import { ExclamationCircleIcon } from "@heroicons/react/outline";
+import {
+  Anchor,
+  TextInput,
+  Button,
+  Group,
+  PasswordInput,
+  Alert,
+} from "@mantine/core";
+import { useForm, yupResolver } from "@mantine/form";
+import { Layout } from "../components/Layout";
+import { AuthForm } from "../types/index.model";
+
+const schema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("No email provided"),
+  password: Yup.string()
+    .required("No password provided")
+    .min(5, "Password should be min 5 chars"),
+});
 
 const Home: NextPage = () => {
+  const { push } = useRouter();
+  const [isRegister, setIsRegister] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+  const form = useForm<AuthForm>({
+    validate: yupResolver(schema),
+    initialValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const handleSubmit = async () => {
+    try {
+      if (isRegister) {
+        await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/signup`, {
+          email: form.values.email,
+          password: form.values.password,
+        });
+      }
+      await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
+        email: form.values.email,
+        password: form.values.password,
+      });
+      form.reset();
+      push("/dashboard");
+    } catch (e: any) {
+      setError(e.response.data.message);
+    }
+  };
   return (
-    <div className="p-20">
-      <h1>Hello Next.js 👋</h1>
-      <Sample child="Hello Child" />
-    </div>
+    <Layout title="Auth">
+      <ShieldCheckIcon className="h-16 w-16 text-blue-500" />
+      {error && (
+        <Alert
+          my="md"
+          variant="filled"
+          icon={<ExclamationCircleIcon />}
+          title="Authorization Error"
+          color="red"
+          radius="md"
+        >
+          {error}
+        </Alert>
+      )}
+      <form onSubmit={form.onSubmit(handleSubmit)}>
+        <TextInput
+          mt="md"
+          id="email"
+          label="Email*"
+          placeholder="example@gmail.com"
+          {...form.getInputProps("email")}
+        />
+        <PasswordInput
+          mt="md"
+          id="password"
+          placeholder="password"
+          label="Password*"
+          description="Must be min 5 char"
+          {...form.getInputProps("password")}
+        />
+        <Group mt="xl" position="apart">
+          <Anchor
+            component="button"
+            type="button"
+            size="xs"
+            className="text-gray-300"
+            onClick={() => {
+              setIsRegister(!isRegister);
+              setError("");
+            }}
+          >
+            {isRegister
+              ? "Have an account? Login"
+              : "Don't have an account? Register"}
+          </Anchor>
+          <Button
+            leftIcon={<IconDatabase size={14} />}
+            color="cyan"
+            type="submit"
+          >
+            {isRegister ? "Register" : "Login"}
+          </Button>
+        </Group>
+      </form>
+    </Layout>
   );
 };
 
